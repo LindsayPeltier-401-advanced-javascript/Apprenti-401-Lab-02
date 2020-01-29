64252273fbf7ef69eef2289573929632451b3057
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-29 04:01:04 -0800

    vehicle classes

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -1,6 +1,42 @@
 'use strict';
 
-class Car {
+/**
+ * Vehicle
+ * @class
+ */
+
+class Vehicle {
+  constructor(name, wheels) {
+    this.name = name;
+    this.wheels = wheels;
+  }
+  drive() {
+    return 'Movement';
+  }
+  stop() {
+    return 'Stopped';
+  }
 }
 
-module.exports = Car;
+/**
+ * Car
+ * @class
+ */
+
+class Car extends Vehicle {
+  constructor(model) {
+    super(name, 4);
+  }
+}
+
+/**
+ * Motorcycle
+ * @class
+ */
+class Motorcycle extends Vehicle {
+  constructor(model) {
+    super(name, 2);
+  }
+}
+
+module.exports = { Car, Motorcycle };
