116ea528893e9c1d1d83f2240511ed5947429ab0
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-29 05:23:44 -0800

    dependencies and index.js

--- classes/index.js
+++ classes/index.js
@@ -1,14 +1,39 @@
+
 'use strict';
 
 const VehicleConstructor = require('./vehicle-constructor.js');
+const VehicleClass = require('./vehicle-class');
 
-// Implement a car and motorcycle using a Constructor
-const mazda = new VehicleConstructor.Car('Mazda 3');
-console.log(mazda.name, mazda.drive(), mazda.stop());
+/**
+ *  Implement a car using a Constructor
+ *  @function
+ *  @param {string} name - Assigns the name to the instance of that vehicle
+ */
+const chevrolet = new VehicleConstructor.Car('Camaro');
+console.log(chevrolet.name, chevrolet.drive(), chevrolet.stop());
 
+/**
+ *  Implement a motorcycle using a Constructor
+ *  @function
+ *  @param {string} name - Assigns the name to the instance of that vehicle
+ */
 const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
 
-// Implement a car and motorcycle using a Class 
+/**
+ * 
+ *  Implement a car using a Class
+ *  @function
+ *  @param {string} name - Assigns the name to the instance of that vehicle
+ */
+const jeep = new VehicleClass.Car('Jeep Compass');
+console.log(jeep.name, jeep.drive(), jeep.stop());
 
-
+/**
+ * 
+ *  Implement a motorcycle using a Class
+ *  @function
+ *  @param {string} name - Assigns the name to the instance of that vehicle
+ */
+const bike = new VehicleClass.Motorcycle('generic bike');
+console.log(bike.name, bike.drive(), bike.stop());
