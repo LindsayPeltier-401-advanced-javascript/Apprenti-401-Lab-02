ddabf239a0b5854203ed049d915491153ce02b6d
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-29 05:52:00 -0800

    used test source code

--- classes/__tests__/vehicle.test.js
+++ classes/__tests__/vehicle.test.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const VehicleConstructor = require('../vehicle-constructor.js');
+const VehicleClass = require('../vehicle-class.js');
 
-let types = ['Constructor', 'Class'];
+let types = ['Constructor'];
+let classTypes = ['Class'];
 
 describe('Vehicles', () => {
 
@@ -12,6 +14,15 @@ describe('Vehicles', () => {
       switch (type) {
         case 'Constructor':
           return new VehicleConstructor.Car('foo');
+        default:
+          return {};
+      }
+    }
+
+    function getCarClass(type) {
+      switch (type) {
+        case 'Class':
+          return new VehicleClass.Car('foo');
         default:
           return {};
       }
@@ -38,6 +49,26 @@ describe('Vehicles', () => {
       });
     });
 
+    classTypes.forEach(type => {
+      let car2 = getCarClass(type);
+
+      it(`${type} (Car) has 4 wheels`, () => {
+        expect(car2.wheels).toEqual(4);
+      });
+
+      it(`${type} (Car) can drive`, () => {
+        expect(car2.drive()).toBeTruthy();
+      });
+
+      it(`${type} (Car) can stop`, () => {
+        expect(car2.stop()).toBeTruthy();
+      });
+
+      it(`${type} (Car) cannot do a wheelie`, () => {
+        expect(car2.wheelie).toBeUndefined();
+      });
+    });
+
   });
 
   describe(`Motorcycle`, () => {
@@ -46,6 +77,15 @@ describe('Vehicles', () => {
       switch (type) {
         case 'Constructor':
           return new VehicleConstructor.Motorcycle('foo');
+        default:
+          return {};
+      }
+    }
+
+    function getMotorcycleClass(type) {
+      switch (type) {
+        case 'Class':
+          return new VehicleClass.Motorcycle('foo');
         default:
           return {};
       }
@@ -69,6 +109,28 @@ describe('Vehicles', () => {
 
       it(`${type} (Motorcycle) cannot do a wheelie`, () => {
         expect(motorcycle.wheelie()).toBeTruthy();
+      });
+
+    });
+
+    classTypes.forEach(type => {
+
+      let motorcycle2 = getMotorcycleClass(type);
+
+      it(`${type} (Motorcycle) has 2 wheels`, () => {
+        expect(motorcycle2.wheels).toEqual(2);
+      });
+
+      it(`${type} (Motorcycle) can drive`, () => {
+        expect(motorcycle2.drive()).toBeTruthy();
+      });
+
+      it(`${type} (Motorcycle) can stop`, () => {
+        expect(motorcycle2.stop()).toBeTruthy();
+      });
+
+      it(`${type} (Motorcycle) cannot do a wheelie`, () => {
+        expect(motorcycle2.wheelie()).toBeTruthy();
       });
 
     });
--- validator/__tests__/validator.test.js
+++ validator/__tests__/validator.test.js
@@ -1,221 +1,150 @@
 'use strict';
 
-const validatorModule = require('../lib/validator.js');
 const faker = require('faker');
-const validatorClass = require('../lib/validatorClass.js');
+const validator = require('../validator');
 
-let types = ['Module', 'Class'];
-
-function getValidator(type) {
-  switch (type) {
-    case 'Module':
-      return validatorModule;
-    case 'Class':
-      return validatorClass;
-    default:
-      return {};
-  }
-}
+let str = 'yes';
+let num = 1;
+let arr = ['a'];
+let obj = { x: 'y' };
+let func = () => { };
+let bool = false;
 
 const schema = {
   fields: {
-    id: {
-      type: 'string',
-      required: true,
-    },
-    name: {
-      type: 'string',
-      required: true,
-    },
-    age: {
-      type: 'number',
-      required: true,
-    },
-    children: {
-      type: 'array',
-      valueType: 'string',
-    },
+    id: { type: 'string', required: true },
+    name: { type: 'string', required: true },
+    age: { type: 'number' },
+    children: { type: 'array', valueType: 'string' },
   },
 };
 
-let string = 'yes';
-let number = 1;
-let array = ['a'];
-let object = {
-  x: 'y',
-};
-let func = () => { };
-let boolean = false;
+describe('Validator module performs basic validation of', () => {
+  it('strings', () => {
+    expect(validator.isString(str)).toBeTruthy();
+    expect(validator.isString(num)).toBeFalsy();
+    expect(validator.isString(arr)).toBeFalsy();
+    expect(validator.isString(obj)).toBeFalsy();
+    expect(validator.isString(func)).toBeFalsy();
+    expect(validator.isString(bool)).toBeFalsy();
+  });
 
-describe('data type validation of', () => {
-  types.forEach(type => {
-    const validator = getValidator(type);
+  it('numbers', () => {
+    expect(validator.isNumber(str)).toBeFalsy();
+    expect(validator.isNumber(num)).toBeTruthy();
+    expect(validator.isNumber(arr)).toBeFalsy();
+    expect(validator.isNumber(obj)).toBeFalsy();
+    expect(validator.isNumber(func)).toBeFalsy();
+    expect(validator.isNumber(bool)).toBeFalsy();
+  });
 
-    it('strings', () => {
-      expect(validator.isString(string)).toBeTruthy();
-      expect(validator.isString(number)).toBeFalsy();
-      expect(validator.isString(array)).toBeFalsy();
-      expect(validator.isString(object)).toBeFalsy();
-      expect(validator.isString(func)).toBeFalsy();
-      expect(validator.isString(boolean)).toBeFalsy();
-    });
+  it('arrays', () => {
+    expect(validator.isArray(str)).toBeFalsy();
+    expect(validator.isArray(num)).toBeFalsy();
+    expect(validator.isArray(arr)).toBeTruthy();
+    expect(validator.isArray(obj)).toBeFalsy();
+    expect(validator.isArray(func)).toBeFalsy();
+    expect(validator.isArray(bool)).toBeFalsy();
+  });
 
-    it('numbers', () => {
-      expect(validator.isNumber(string)).toBeFalsy();
-      expect(validator.isNumber(number)).toBeTruthy();
-      expect(validator.isNumber(array)).toBeFalsy();
-      expect(validator.isNumber(object)).toBeFalsy();
-      expect(validator.isNumber(func)).toBeFalsy();
-      expect(validator.isNumber(boolean)).toBeFalsy();
-    });
+  it('arrays of type', () => {
+    let numArray = [1, 2, 3];
+    let strArray = ['a', 'b', 'c'];
 
-    it('arrays', () => {
-      expect(validator.isArray(string)).toBeFalsy();
-      expect(validator.isArray(number)).toBeFalsy();
-      expect(validator.isArray(array)).toBeTruthy();
-      expect(validator.isArray(object)).toBeFalsy();
-      expect(validator.isArray(func)).toBeFalsy();
-      expect(validator.isArray(boolean)).toBeFalsy();
-    });
+    expect(validator.isArray(str)).toBeFalsy();
+    expect(validator.isArray(num)).toBeFalsy();
+    expect(validator.isArray(obj)).toBeFalsy();
+    expect(validator.isArray(func)).toBeFalsy();
+    expect(validator.isArray(bool)).toBeFalsy();
+    expect(validator.isArray(arr)).toBeTruthy();
+    expect(validator.isArray(numArray, 'number')).toBeTruthy();
+    expect(validator.isArray(numArray, 'string')).toBeFalsy();
+    expect(validator.isArray(strArray, 'string')).toBeTruthy();
+    expect(validator.isArray(strArray, 'number')).toBeFalsy();
+  });
 
-    it('objects', () => {
-      expect(validator.isObject(string)).toBeFalsy();
-      expect(validator.isObject(number)).toBeFalsy();
-      expect(validator.isObject(array)).toBeFalsy();
-      expect(validator.isObject(object)).toBeTruthy();
-      expect(validator.isObject(func)).toBeFalsy();
-      expect(validator.isObject(boolean)).toBeFalsy();
-    });
+  it('objects', () => {
+    expect(validator.isObject(str)).toBeFalsy();
+    expect(validator.isObject(num)).toBeFalsy();
+    expect(validator.isObject(arr)).toBeFalsy();
+    expect(validator.isObject(obj)).toBeTruthy();
+    expect(validator.isObject(func)).toBeFalsy();
+    expect(validator.isObject(bool)).toBeFalsy();
+  });
 
-    it('booleans', () => {
-      expect(validator.isBoolean(string)).toBeFalsy();
-      expect(validator.isBoolean(number)).toBeFalsy();
-      expect(validator.isBoolean(array)).toBeFalsy();
-      expect(validator.isBoolean(object)).toBeFalsy();
-      expect(validator.isBoolean(func)).toBeFalsy();
-      expect(validator.isBoolean(boolean)).toBeTruthy();
-    });
+  it('booleans', () => {
+    expect(validator.isBoolean(str)).toBeFalsy();
+    expect(validator.isBoolean(num)).toBeFalsy();
+    expect(validator.isBoolean(arr)).toBeFalsy();
+    expect(validator.isBoolean(obj)).toBeFalsy();
+    expect(validator.isBoolean(func)).toBeFalsy();
+    expect(validator.isBoolean(bool)).toBeTruthy();
+  });
 
-    it('functions', () => {
-      expect(validator.isFunction(string)).toBeFalsy();
-      expect(validator.isFunction(number)).toBeFalsy();
-      expect(validator.isFunction(array)).toBeFalsy();
-      expect(validator.isFunction(object)).toBeFalsy();
-      expect(validator.isFunction(func)).toBeTruthy();
-      expect(validator.isFunction(boolean)).toBeFalsy();
-    });
+  it('functions', () => {
+    expect(validator.isFunction(str)).toBeFalsy();
+    expect(validator.isFunction(num)).toBeFalsy();
+    expect(validator.isFunction(arr)).toBeFalsy();
+    expect(validator.isFunction(obj)).toBeFalsy();
+    expect(validator.isFunction(func)).toBeTruthy();
+    expect(validator.isFunction(bool)).toBeFalsy();
   });
 });
 
-describe('schema validation', () => {
-  types.forEach(type => {
-    const validator = getValidator(type);
-    it('isValid() function validates a good record.', () => {
-
-      let testRecord = {};
-
-      for (let field in schema.fields) {
-        switch (schema.fields[field].type) {
-          case 'boolean':
-            testRecord[field] = faker.random.boolean();
-            break;
-          case 'number':
-            testRecord[field] = faker.random.number();
-            break;
-          case 'string':
-            testRecord[field] = faker.random.word();
-            break;
-          case 'array':
-            testRecord[field] = [];
-            testRecord[field].push(faker.random.arrayElement());
-            testRecord[field].push(faker.random.arrayElement());
-            break;
-          default:
-            null;
-        }
+describe('Validator module evaluates a basic schema', () => {
+  it('isValid() validates a good record', () => {
+    let testRecord = {};
+    for (let field in schema.fields) {
+      switch (schema.fields[field].type) {
+        case 'boolean':
+          testRecord[field] = faker.random.boolean();
+          break;
+        case 'number':
+          testRecord[field] = faker.random.number();
+          break;
+        case 'string':
+          testRecord[field] = faker.random.word();
+          break;
+        case 'array':
+          testRecord[field] = [];
+          testRecord[field].push(faker.random.arrayElement());
+          testRecord[field].push(faker.random.arrayElement());
+          break;
+        default:
+          null;
       }
-      expect(validator.isValid(schema, testRecord)).toBeTruthy();
-    });
+    }
 
-    it('isValid() function returns undefined with missing requirements', () => {
-
-      let testRecord = {};
-      for (let field in schema.fields) {
-        if (schema.fields[field].required) {
-          testRecord[field] = null;
-        }
-      }
-      expect(validator.isValid(schema, testRecord)).toBeFalsy();
-    });
+    expect(validator.isValid(schema, testRecord)).toBeTruthy();
   });
-});
 
-const personRules = {
-  fields: {
-    id: {
-      type: 'string',
-      required: true,
-    },
-    name: {
-      type: 'string',
-      required: true,
-    },
-    age: {
-      type: 'number',
-      required: true,
-    },
-    children: {
-      type: 'array',
-      valueType: 'string',
-    },
-  },
-};
+  it('isValid() returns undefined on type mismatch', () => {
+    let testRecord = {};
+    for (let field in schema.fields) {
+      switch (schema.fields[field].type) {
+        case 'boolean':
+          testRecord[field] = faker.random.number();
+          break;
+        case 'number':
+          testRecord[field] = faker.random.word();
+          break;
+        case 'string':
+          testRecord[field] = faker.random.number();
+          break;
+        default:
+          null;
+      }
+    }
+    expect(validator.isValid(schema, testRecord)).toBeFalsy();
+  });
 
-const Bob = {
-  id: '123-45-6789',
-  name: 'Bob',
-  age: 40,
-  children: [],
-};
-
-const Sally = {
-  id: 19,
-  name: 'Sally Sally',
-  children: [],
-};
-
-const Howard = {};
-
-const arrayRules = {
-  fields: {
-    test: {
-      type: 'array',
-      valueType: 'number',
-    },
-  },
-};
-
-const Lola = {
-  test: ['Lola ', 34],
-};
-
-describe('validator module performs complex validations', () => {
-  types.forEach(type => {
-    const validator = getValidator(type);
-    it('Validates an object with a set of rules against an object', () => {
-      expect(validator.isValid(personRules, Bob)).toBeTruthy();
-    });
-
-    it('validates an object with a set of rules against an object that does not pass', () => {
-      expect(validator.isValid(personRules, Sally)).toBeFalsy();
-    });
-
-    it('does not validate rules against an empty object', () => {
-      expect(validator.isValid(personRules, Howard)).toBeFalsy();
-    });
-
-    it('validates a value array against an approved list', () => {
-      expect(validator.isValid(arrayRules, Lola)).toBeFalsy();
-    });
+  it('isValid() returns undefined with missing requirements', () => {
+    let testRecord = {};
+    for (let field in schema.fields) {
+      if (schema.fields[field].required) {
+        testRecord[field] = null;
+      }
+    }
+    expect(validator.isValid(schema, testRecord)).toBeFalsy();
   });
 });
